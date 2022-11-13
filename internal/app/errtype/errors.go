f28eae160aa61bd6198a17822f4b6af3a5709aff
beldeveloper <beldeveloper@gmail.com>
2022-11-13 04:30:15 +0100

    chore: remove unused error type

--- internal/app/errtype/errors.go
+++ internal/app/errtype/errors.go
@@ -9,6 +9,4 @@ var (
 	ErrBadInput = errors.New("bad input")
 	// ErrUnauthorized represents the error for the cases when the authorization is required.
 	ErrUnauthorized = errors.New("unauthorized")
-	// ErrBuildCanceled represents the error for the case when the build process was automatically canceled.
-	ErrBuildCanceled = errors.New("build canceled")
 )
